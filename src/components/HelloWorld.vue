3e1ce9e1b98547ee8a55f47f5bb09a871a895f0d
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-01-25 21:45:54 +0100

    Add Test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify Test</h1>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
