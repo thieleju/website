3e461a51fd56b8d4b29e600c1ab08cc9cfbf852a
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-01-25 22:26:15 +0100

    Add buttons to switch between routes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="12">
+        <h1>This is an about page</h1>
+
+        <v-btn @click="routeToName('Home')">Go to Home</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  methods: {
+    routeToName(name) {
+      this.$router.push({ name });
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="12">
+        <h1>This is the Home page</h1>
+
+        <v-btn @click="routeToName('About')">Go to about</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
   name: "Home",
 
-  components: {
-    HelloWorld,
+  components: {},
+  methods: {
+    routeToName(name) {
+      this.$router.push({ name });
+    },
   },
 };
 </script>
